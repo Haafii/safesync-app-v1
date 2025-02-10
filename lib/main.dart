6737eae57e58c7ff38486914017973d0d601f1ac
Haafii <hafismuhammedtk@gmail.com>
2025-02-10 21:30:09 +0530

    Remove commented-out code in main.dart for improved clarity

--- lib/main.dart
+++ lib/main.dart
@@ -26,33 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-// import 'screens/splash_screen.dart'; // Import the splash screen
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         useMaterial3: true,
-//       ),
-//       home: const SplashScreen(), // Set the splash screen as the initial screen
-//     );
-//   }
-// }
